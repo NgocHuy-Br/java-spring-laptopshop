d103bf6b1cf893b54c55fa7e31c5b84da328451f
NgocHuy-Br <ngochuy.mep@gmail.com>
2025-03-31 22:23:28 +0800

    get post method

--- src/main/java/vn/hoidanit/laptopshop/service/UserService.java
+++ src/main/java/vn/hoidanit/laptopshop/service/UserService.java
@@ -5,6 +5,6 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserService {
     public String handleHello() {
-        return "Huy hello from controller";
+        return "Huy pro hello from controller";
     }
 }
